68e5645c9a08826a9a127270b1ada794081c63fd
Souvik Roy <souvikroy1511@gmail.com>
2022-06-19 03:59:18 +0530

    change

--- summing.java
+++ summing.java
@@ -3,8 +3,8 @@ public class summing
 	public static void main(String[] args)
 	{
 		int a=10;
-		int b=25;
+		int b=20;
 		int c=a+b;
-		System.out.println(c);
+		System.out.println("sum"+c);
 	}
 }
